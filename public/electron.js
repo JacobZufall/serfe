062c1124e5bc759b6c8f8c315c75fd0242debb3c
Jacob Zufall <jacobzufall@gmail.com>
2024-02-09 00:33:01 -0500

    Laying foundation for the app in App.tsx

--- public/electron.js
+++ public/electron.js
@@ -7,17 +7,34 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 function createWindow() {
-    // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: { nodeIntegration: true, contextIsolation: false },
     });
-    // and load the index.html of the app.
-    console.log(__dirname);
+
+    // Removes the menu bar from the top of the window.
+    mainWindow.removeMenu();
+    // Loads the built file.
     mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
+    // Handles the user closing the application.
+    mainWindow.on("closed", () => {mainWindow = null;});
 }
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
+
 app.on("ready", createWindow);
+
+// If all the browser windows are closed, the application will be explicitly quit.
+app.on("window-all-closed", () => {
+   if (process.platform !== "darwin") {
+       app.quit();
+   }
+});
+
+// On macOS, in the event all windows are closed, this will create a new window when the user clicks on the icon in the
+// dock.
+app.on("activate", () => {
+   if (mainWindow === null) {
+       createWindow();
+   }
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
+function Workspace({newSelection}: {newSelection: number}) {
+  // Each workspace should have its own case, which is imported from another file.
+  switch (newSelection) {
+    case 1:
+      return (
+          <h1>Case 1</h1>
+      );
+
+    // For safety, we'll default to the dashboard, which should be thought of as "case 0".
+    default:
+      return (
+          <h1>Case 0</h1>
+      );
+  }
+}
+
+// The main application.
 function App() {
+  const [selection, setSelection]: [number, (value: (((prevState: number) => number) | number)) => void] = useState(0);
+
+  function updateSelection(newSelection: number) {
+    setSelection(newSelection);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <body>
+        <div className="wrapper">
+          <nav>
+            <button className="navButton" onClick={() => updateSelection(0)}>A</button>
+            <button className="navButton" onClick={() => updateSelection(1)}>B</button>
+          </nav>
+
+          <div id="workspace">
+            <div id="safezone">
+              <Workspace newSelection={selection} />
+            </div>
+          </div>
+        </div>
+      </body>
   );
 }
 
